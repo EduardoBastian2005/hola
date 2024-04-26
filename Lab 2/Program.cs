a8c0d20ccb110c3d985267a2d3726e03ae0714f4
EduardoBastian2005 <167550268+EduardoBastian2005@users.noreply.github.com>
2024-04-26 11:10:11 -0500

    Agregar archivos de proyecto.

---
+++ Lab 2/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//Ejercicios del lab 2 (no tarea)
+// Suma de numeros reales
+var n = 2;
+var suma = 0;
+for (var i = 1; i <= n; i++)
+{
+    suma += i;
+}
+Console.WriteLine("La suma de los " + n + " números naturales es: " + suma);
